3de31300d40b935228b2c9924031a16346a618c7
Dan Nawara <dan@oneis.us>
2016-07-27 20:40:15 -0500

    Ignore repeat request for 1 minute

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 var config = require('./config'),
     dash_button = require('node-dash-button'),
     logger = require('./lib/loggingService'),
+    presses = [],
     request = require('request');
 
 config.buttons.forEach(function(button){
   var dash = dash_button(button.id);
   dash.on("detected", function (){
-    doAction[button.action](button);
+    if(!presses[button.id]){
+      doAction[button.action](button);
+      presses[button.id] = true;
+    };
+    setTimeout(function(){presses[button.id]=false},60000)
   });
 });
 
